2667f8d4a8bdc0ed02148c18e11434e645b11633
EvilJ84 <evilj84@gmail.com>
2013-05-22 18:44:24 +0200

    Added Account.cs

---
+++ BankApp/BankApp/Model/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.View
+{
+    class Account
+    {
+        public string AccountID;
+        public decimal Amount;
+        public decimal Interest;
+    }
+}
